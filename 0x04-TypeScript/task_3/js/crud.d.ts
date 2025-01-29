3619ba880a5dd89388ef87f0380204d17066f79e
Sandrah <comfortsandrah13@gmail.com>
2025-01-29 23:01:18 +0300

    Add TypeScript interfaces and CRUD operations for managing rows

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+import {RowId,RowElement} from "./interface.ts";
+export function insertRow(row: RowElement): number; 
+  
+export function deleteRow(rowId: RowId): void; 
+  
+export function updateRow(rowId:RowId, row:RowElement): RowElement; 
---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,8 @@
+namespace Subjects{
+    export class Subject implements Teacher{
+        firstName: string;
+        lastName: string;
+        // setTeacher(): 
+
+    }
+}
